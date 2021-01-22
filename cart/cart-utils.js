8751e0f2ae482d53af5dad960eb36a70d02639a2
Lori Winston <Lcwinston@gmail.com>
2021-01-22 11:44:16 -0800

    link working

---
+++ cart/cart-utils.js
@@ -0,0 +1,54 @@
+import { cart } from './cart-data';
+
+const CART = 'CART';
+const defaultEmptyCart = [];
+import { findById } from '../utils.js';
+
+
+function getCart() {
+    const stringyCart = localStorage.getItem(CART);
+
+    if (stringyCart) {
+        const parsedCart = JSON.parse(stringyCart);
+    
+        return parsedCart;
+    } else {
+        const stringyDefaultCart = JSON.stringify(defaultEmptyCart);
+        localStorage.setItem(CART, stringyDefaultCart);
+
+
+
+        return defaultEmptyCart;
+
+    }
+}
+
+export function clearCart() {
+    const stringyDefaultCart = JSON.stringify(defaultEmptyCart);
+    localStorage.setItem(CART, stringyDefaultCart);
+
+}
+
+export function setCart(cart) {
+    const stringyCart = JSON.stringify(cart);
+    localStorage.setItem(CART, stringyCart);
+}
+
+export function addToCart(id) {
+    const cart = getCart();
+
+    const cartItem = findById(id, cart);
+
+    if (cartItem) {
+        cartItem.quantity++;
+    } else {
+        const newItem = {
+            id: id,
+            quantity: 1
+        };
+
+        cart.push(newItem);
+    }
+
+    setCart(cart);
+}
